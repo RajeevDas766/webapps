59da2c234186ed43be22239d6d2e555c95dbe236
RajeevDas766 <rajeevdas766@gmail.com>
2024-09-09 11:39:32 +0530

    Webapp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenHttpInterceptor } from './core/token-http-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes),
+     provideAnimationsAsync(),
+    provideHttpClient(),
+    provideHttpClient(withInterceptors([tokenHttpInterceptor])),
+    
+  ]
 };
